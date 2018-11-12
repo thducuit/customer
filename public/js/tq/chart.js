3dacd8bf6c5b457536587f6b17441573980b19ba
Nguyen Thanh Duc（y335415） <ty335415@toyo-local.co.jp>
2018-11-12 10:47:02 +0700

    fix js chart

--- public/js/tq/chart.js
+++ public/js/tq/chart.js
@@ -21,6 +21,33 @@ function TQ_Chart(elementID, title, chart_data, labels) {
             },
             scales: {
                 yAxes: chart_data['yAxes']
+            },
+            hover: {
+                animationDuration: 0
+            },
+            animation: {
+                duration: 1,
+                onComplete: function () {
+                    var chartInstance = this.chart,
+                        ctx = chartInstance.ctx;
+                    
+                    ctx.font = Chart.helpers.fontString(Chart.defaults.global.defaultFontSize, Chart.defaults.global.defaultFontStyle, Chart.defaults.global.defaultFontFamily);
+                    ctx.textAlign = 'center';
+                    ctx.textBaseline = 'bottom';
+
+                    this.data.datasets.forEach(function (dataset, i) {
+                        var meta = chartInstance.controller.getDatasetMeta(i);
+                        meta.data.forEach(function (bar, index) {
+                            var data = dataset.data[index]; 
+                            if(data) {
+                                ctx.fillText(data, bar._model.x, bar._model.y - 5);
+                            }   
+                        });
+                    });
+                }
+            },
+            tooltips: {
+                "enabled": false
             }
         }
     });
